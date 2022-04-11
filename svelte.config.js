4d29330aacc754cb5b7a77900b92765bec5b4342
nightconcept <dark@nightconcept.net>
2022-04-10 17:51:29 -0700

    Svelte config test

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex'
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
 		preprocess(),
 		mdsvex({
-			extensions: ['.md'],
+			extensions: ['.md', '.svx'],
 			layout: {
-        blog: 'src/routes/blog/_post.svelte'
-      }
+				garden: './src/routes/garden/_post.svelte',
+				_: './src/mdsvexlayout.svelte'
+			}
 		})
 	],
 
-	extensions: ['.svelte', '.md'],
-
-	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+	extensions: ['.svelte', '.md', '.svx'],
 };
 
 export default config;
