4b638b2d9a698af7c19360ef71ee6f6c32dedf84
Danny Solivan <dark@nightconcept.net>
2023-04-25 09:01:55 -0700

    Add uses redirect

---
+++ src/routes/uses/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+    if ( browser ) {
+        goto('/blog/uses');
+    }
+</script>
+
+<svelte:head>
+	<title>/uses</title>
+</svelte:head>
