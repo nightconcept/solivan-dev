475de0a3ca15bdb68c71c5c12999e44c31ebfb8b
Danny Solivan <dark@nightconcept.net>
2023-04-24 17:09:40 -0700

    bump deps, clean comments

--- src/routes/api/posts.json/+server.ts
+++ src/routes/api/posts.json/+server.ts
@@ -1,25 +1,3 @@
-// returns posts in descending order
-// probably need to update this to not return so many files as this gets too big
-
-// export const get = async () => {
-// 	let posts = await Promise.all(
-// 		Object.entries(import.meta.glob('../garden/*.md')).map(async ([path, page]) => {
-// 			const { metadata } = await page();
-// 			const slug = path.split('/').pop().split('.').shift();
-// 			return { ...metadata, slug };
-// 		})
-// 	);
-
-// 	return {
-// 		status: 200,
-// 		body: {
-// 			posts: posts.sort(
-// 				(a, b) => new Date(b.date).setHours(0, 0, 0, 0) - new Date(a.date).setHours(0, 0, 0, 0)
-// 			)
-// 		}
-// 	};
-// };
-
 import { parse } from 'path';
 
 type GlobEntry = {
